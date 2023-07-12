cfafbe9704dd706cb54edd81a5fafd7d672dc6d6
RayanAnser <rayan.anser@hotmail.fr>
2023-07-12 13:48:50 +0200

    text anim

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
-
+import AnimationController from './controllers/animation_controller';
 const application = Application.start()
+application.register('animation', AnimationController);
+
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
 
 export { application }
