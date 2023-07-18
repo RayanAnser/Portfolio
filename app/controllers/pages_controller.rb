cb62398be90b5db1c0e42e4baf4af420bac265dc
RayanAnser <rayan.anser@hotmail.fr>
2023-07-18 20:34:23 +0200

    added menu improvement

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    def section1
+    end
+
+    def section2
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+Rails.application.routes.draw do
+  get '/section1', to: 'pages#section1'
+  get '/section2', to: 'pages#section2'
+end
+
 end
